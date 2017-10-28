f8781d23afcdced54239828752e66e263e797725
Sam Rae <samuelrae@gmail.com>
2017-10-28 23:08:58 +0100

    gets movie titles and posters displaying using hardcoded config vars

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,75 @@
 import * as React from 'react'
 import './App.css'
-import { TmdbApi } from 'tmdb-typescript-api'
+import { Context, TmdbApi, SearchResult, Movie } from 'tmdb-typescript-api'
+import { Subject } from 'rxjs'
+
 const API_KEY = process.env.TMDB_API_KEY_V3 || ''
+const BASE_IMAGE_URL = 'https://image.tmdb.org/t/p/'
+const POSTER_SIZE = 'w150'
 
-console.log('api', API_KEY)
+interface IAppState {
+  results: Movie[]
+}
 
-const logo = require('./logo.svg')
+class App extends React.Component<{}, IAppState> {
 
-class App extends React.Component {
+  searchTerm: Subject<{}>
+  api: TmdbApi
+
+  constructor () {
+    super()
+    this.state = {
+      results: []
+    }
+    this.searchTerm = new Subject()
+    this.api = new TmdbApi(API_KEY)
+  }
+
   componentDidMount () {
-    const api: TmdbApi = new TmdbApi(API_KEY)
-
-    api.search.movies('Pulp Fiction').subscribe(movies => {
-      let movie = movies.results[0]
-      console.log(`'Pulp Fiction' was released in ${movie.release_date}`)
+    // TODO catch error
+    this.searchTerm
+      .throttleTime(400)
+      .switchMap((term: string) => this.api.search.movies(term))
+      .subscribe((m: SearchResult<Movie>) => {
+        console.log(m.results)
+        this.setState({
+        results: m.results
+      })
     })
+  }
 
-    api.tvshows.details(4607).subscribe(show => {
-      console.log(`'Lost' had ${show.number_of_seasons} seasons`)
-    })
+  search = (event: React.ChangeEvent<HTMLInputElement>) => {
+    if (event.target.value.length < 2) {
+      this.setState({results: []})
+    } else {
+      this.searchTerm.next(event.target.value)
+    }
   }
 
   render () {
+    console.log('baseurl', Context)
     return (
       <div className='App'>
         <div className='App-header'>
-          <img src={logo} className='App-logo' alt='logo' />
-          <h2>Welcome to React</h2>
+          <h2>Search the Movie Database</h2>
+          <input type='text' onChange={this.search}></input>
         </div>
-        <p className='App-intro'>
-          To get started, edit <code>src/App.tsx</code> and save to reload.
-        </p>
+        <ul>
+          {this.state.results.map((result: Movie) => <MovieCard key={result.id} {...result}/>)}
+        </ul>
+    
       </div>
     )
   }
+}
+
+export const MovieCard = (props: Movie) => {
+  return (
+    <div key={props.id}>
+      <li>{props.title}</li>
+      <img src={`${BASE_IMAGE_URL}${POSTER_SIZE}${props.poster_path}`} alt={props.title} />
+    </div>
+  )
 }
 
 export default App
