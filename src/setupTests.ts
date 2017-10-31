5a0d45c976f790799bc8eedb5572f00c6206397e
Sam Rae <samuelrae@gmail.com>
2017-10-31 01:52:45 +0000

    upgraded react-scripts

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -1,5 +1,6 @@
 // TODO find way to fix this warning:
 // https://github.com/facebookincubator/create-react-app/issues/3199
+// without breaking tests for ApplicationCache.tsx
 
 import * as Enzyme from 'enzyme';
 import * as Adapter from 'enzyme-adapter-react-16'
